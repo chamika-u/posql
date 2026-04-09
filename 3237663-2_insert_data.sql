ece7a6708cb07b4831201250b662da694d8c59e1
Chamika Udayanga <c.udayanga2k04@gmail.com>
2026-04-09 01:04:03 +0000

    Update query `2_insert_data`

--- 3237663-2_insert_data.sql
+++ 3237663-2_insert_data.sql
@@ -1,6 +1,6 @@
 -- Insert data into students table
 insert into students(student_name) values ('Chamika'), ('Dinethma'), ('Minuthi'),('Yoshitha'), ('Chanul'),('Maleesha'),('Dilush');
-SELECT * FROM students;
+select * from s
 
 -- Insert data into streams table
 insert into subjectStreams(stream_name) values ('Physical Sciences'), ('Biology'), ('Commerce'),('Engineering Technology'),('Bio System Technology'),('Arts');
