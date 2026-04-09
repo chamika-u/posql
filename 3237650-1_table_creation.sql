60014d728acc86ef12ca1dbfde0d170e62cb8f26
Chamika Udayanga <c.udayanga2k04@gmail.com>
2026-04-09 00:45:12 +0000

    Move query `1_table_creation` (part 2/2)

---
+++ 3237650-1_table_creation.sql
@@ -0,0 +1,13 @@
+create table students(
+	student_id serial primary key,
+	student_name varchar(100)
+);
+
+create table subjectStreams(
+    stream_id serial primary key,
+	stream_name varchar(100)
+);
+
+create table subjects(
+	subject
+)
